04d8252426240fcdd07e85b28baabe4f3caa0e0b
ehju <charmjeh@gmail.com>
2019-11-06 01:35:01 +0900

    add controller

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 import express from "express";
-import morgan from "morgan";
-import helmet from "helmet"
-import cookieParser from "cookie-parser"
-import bodyParser from "body-parser"
-import { userRouter } from "./router"
+import morgan from "morgan"; // 애플리케이션에서 발생하는 모든 일들을 logging함
+import helmet from "helmet" // 애플리케이션이 더 안전하도록 함
+import cookieParser from "cookie-parser" // cookie를 전달받아서 사용할 수 있도록함
+import bodyParser from "body-parser" // 사용자가 웹사이트로 전달하는 정보들을 검사함
+import globalRouter from "./routers/globalRouter"
+import userRouter from "./routers/userRouter"
+import videoRouter from "./routers/videoRouter"
+import routes from './routes'
 
 const app = express();
-
-const handleHome = (req, res) => res.send('hello from /')
-const handleProfile = (req, res) => res.send('hello from profile')
 
 app.use(cookieParser())
 app.use(bodyParser.json())
@@ -16,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(helmet());
 app.use(morgan("dev"));
 
-app.get('/', handleHome);
-app.get('/profile', handleProfile);
-app.use('/user', userRouter); // use를 사용함으로써 /user에 접속 시 userRouter 전체를 사용하게 됨.
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
