92af5bf0415d26687a5338a0e47c6e832772797d
ehju <charmjeh@gmail.com>
2019-11-07 00:37:46 +0900

    add Mixin

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos = [
+    {
+        id: 3236,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24, 
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@las.com"
+        }
+    },
+    {
+        id: 3237,
+        title: 'Video super',
+        description: 'Changed description',
+        views: 24, 
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@las.com"
+        }
+    },
+    {
+        id: 3238,
+        title: 'Video nice',
+        description: 'This is something I love',
+        views: 24, 
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 121213,
+            name: "Nicolas",
+            email: "nico@las.com"
+        }
+    },
+];
