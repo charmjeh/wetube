fe155c0b1f4708bf6560e8bb95c6a41b03b56bf5
ehju <charmjeh@gmail.com>
2020-11-17 00:04:59 +0900

    feat: video player css 추가

--- middlewares.js
+++ middlewares.js
@@ -21,7 +21,7 @@ const multerAvatar = multer({
 export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = "WeTube";
     res.locals.routes = routes;
-    res.locals.loggedUser = req.user || null;
+    res.locals.loggedUser = req.user || {};
     next();
 };
 
